65088bed1a9285bee24b48500ec862a548feb441
Aldeirfrancisco <dide123francisco@gmail.com>
2020-06-07 18:36:49 -0300

    formatacao do template

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+function acao(obj){
+    if(document.getElementById(obj).style.display == 'block'){
+
+    document.getElementById(obj).style.display = 'none';
+
+    }else{
+        document.getElementById(obj).style.display = 'block'; 
+    }
+}
