074752feff82b20343638e5c1741c2e86a5e739e
Aldeirfrancisco <dide123francisco@gmail.com>
2020-06-07 22:17:34 -0300

    fiz um pouco do header e fiz duas funcoes js para o menu

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,27 @@
-function acao(obj){
+
+function acao(obj,foto){
     if(document.getElementById(obj).style.display == 'block'){
+        
+        document.getElementById(foto).style.display = 'block'; 
+        document.getElementById(obj).style.display = 'none';
+     
 
-    document.getElementById(obj).style.display = 'none';
-
-    }else{
+    }else{ 
         document.getElementById(obj).style.display = 'block'; 
+        document.getElementById(foto).style.display = 'none'; 
+
     }
-}
+    }
+    function acaoFotos(obj,foto){
+        if(document.getElementById(obj).style.display == 'block'){
+    
+            document.getElementById(foto).style.display = 'none';
+         
+    
+        }else{ 
+            document.getElementById(foto).style.display = 'block'; 
+            document.getElementById(obj).style.display = 'none'; 
+    
+        }
+        }
+
