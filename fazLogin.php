fe674569755bf879febadbe10ad0bed4736b1cdf
Aldeirfrancisco <dide123francisco@gmail.com>
2020-06-13 19:50:22 -0300

    busca para o login e parte da paagina logado

--- fazLogin.php
+++ fazLogin.php
@@ -1,4 +1,33 @@
 <?php
  session_start();
 
- 
+ $email=  $_GET['email'];
+ $cpf=   $_GET['cpf'];
+
+ $Host ="sql10.freesqldatabase.com";
+ $name = "sql10348173";
+ $user = "sql10348173";
+ $password = "uGSPTJuKp4";
+
+
+
+$conexao =new mysqli($Host, $user,  $password,$name); 
+
+
+$tenta_achar = "SELECT * FROM usuario WHERE  cpf='$cpf'  AND email='$usuario'" ;
+$resultado = $conn->query($tenta_achar);
+if ($resultado->num_rows > 0) {
+    $_SESSION['cpf'] = $cpf;
+    $_SESSION['email'] = $email;
+    echo "<script>
+            window.location.href = 'logado.html';
+        </script>";
+}
+else{
+    session_unset();//remove todas as variáveis de sessão
+    session_destroy();//destroi a sessão
+    echo "<script> 
+            alert('E-mail ou CPF incorreto');
+            window.location.href = 'paginaPrincipal.php';
+        </script>";
+  }
